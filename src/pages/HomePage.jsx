0d2d544f9ce264255d7ed3798e774ac4c6699478
MelodySpring <messagemelody@icloud.com>
2026-07-23 16:39:39 +0100

    Everything on the page prior to styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,24 @@
-// HomePage.js
 import React from 'react';
 import redcode from '../assets/redcode.png';
+import collage from '../assets/collage.png';
 import '../HomePage.css'
 
 const HomePage = () => {
   return (
     <div>
-      <h1 className='mainTitle'>Welcome to the Home Page</h1>
-      <img src={redcode} alt="Red Dog Question" className="redCode" />
-      <p>WOOF WOOF, gather round, hoomans - it’s Red the Malamute, your fluffy marketing manager with excellent fur and even better opinions. I’ve got breaking news straight from the doghouse. My mum - yes, the clever treat dispensing legend herself - has started building websites for people, and she even named her business Red Dog Design after ME. Finally, the recognition I deserve.
-        She has almost finished this exciting Bootcamp with a pack called Step8Up, where she built all sorts of magical hooman projects. Buttons, pages, fancy layouts… honestly, I’m shocked she didn’t chew the keyboard. And her tutor, Jason, is absolutely brilliant. Like, scary brilliant. Even though he likes cats. I mean… nobody’s perfect.
-        So if you need a website, my mum’s ready. I’ll supervise every project with professional tail wags and high‑quality howls.</p>
-    </div>
+      <h1 className='mainTitle'>Red Dog Design</h1>
+      <div className='homePageText'>
+        <p>WOOF WOOF, gather round, hoomans - it’s Red the Malamute, your fluffy marketing manager with excellent fur and even better opinions. I’ve got breaking news straight from the doghouse. My mum - yes, the clever treat dispensing legend herself - has started building websites for people, and she even named her business Red Dog Design after ME. Finally, the recognition I deserve. </p>
+        <img src={redcode} alt="Image of a malamute with snippets of code round its head" className="redCode" />
+      </div>
+      <div className='homePageText'>
+        <img src={collage} alt="A collage of web pages" className="collage" />
+        <p>She has almost finished this exciting Bootcamp with a pack called Step8Up, where she built all sorts of magical hooman projects. Javascript, Bootstrap, fancy layouts… honestly, I’m shocked she didn’t chew the keyboard just don't mention Python. And her tutor, Jason, is absolutely brilliant. Like, scary brilliant. Even though he likes cats. I mean… nobody’s perfect.</p>
+      </div>
+      <div>
+        <p>So if you need a website, my mum’s ready. I’ll supervise every project with professional tail wags and high‑quality howls.</p>
+      </div>
+    </div >
   );
 };
 
--- src/pages/RandomRedPage.jsx
+++ src/pages/RandomRedPage.jsx
@@ -1,20 +1,34 @@
-// HomePage.js
 import React from 'react';
 import RandomRed from "../components/randomred";
 import reddogblock from '../assets/reddogblock.png';
 import '../RandomRed.css';
 
-
-const HomePage = () => {
+const RandomRedPage = () => {
   return (
     <div>
       <h1 className='randomRedTitle'>Random Red</h1>
-      <p>Woof! Hey there, hooman! It’s me, Red the Malamute, your fluffy game master. I’ve been working on this pawsome block game, and guess what? Every square wants to be red—just like moi! I tried blue once, but it looked sad, like a cold puddle. Red is bold, warm, and screams “I’m fabulous!” So grab your paws-uh, hands-and help me turn every block crimson. If you win, I’ll give you a celebratory tail wag and maybe a slobbery kiss. If you lose… well, you’ll still get the slobbery kiss. Let’s make the world red together! .</p>
-      <img src={reddogblock} alt="Red Dog Block" className="redBlockImage" />
+      <div className='randomRedText'>
+        <p>
+          Woof! Hey there, hooman! It’s me, Red the Malamute, your fluffy game master.
+          I’ve been working on this pawsome block game, and guess what? Every square
+          wants to be red—just like moi! I tried blue once, but it looked sad, like a
+          cold puddle. Red is bold, warm, and screams “I’m fabulous!” So grab your
+          paws-uh, hands-and help me turn every block crimson. If you get the fastest
+          time you win, I’ll give you a celebratory tail wag and maybe a slobbery kiss.
+          If you lose… well, you’ll still get the slobbery kiss. Let’s make the world
+          red together!
+        </p>
 
+
+        <img
+          src={reddogblock}
+          alt="image of a malamute with coloured blocks round its head"
+          className="redBlockImage" />
+      </div>
       <RandomRed />
     </div>
   );
 };
 
-export default HomePage;
+export default RandomRedPage;
+
